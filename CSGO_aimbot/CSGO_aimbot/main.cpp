3afcee6f98024933ca91509afd945a28c57cf092
Olivier St-Amour <olivierstamour0952@gmail.com>
2015-08-01 19:11:26 -0400

    some implementations started

--- CSGO_aimbot/CSGO_aimbot/main.cpp
+++ CSGO_aimbot/CSGO_aimbot/main.cpp
@@ -4,5 +4,17 @@
 
 int main()
 {
+	MemoryManager* memoryManager = new MemoryManager();
+	AimBot* aimBot = new AimBot(memoryManager);
+	if (!memoryManager->init())
+	{
+		cout << "Error cannot initialize memory manager." << endl;
+	}
+	else
+	{
+		aimBot->updateData();
+	
+	}
+
 	return 0;
 }
